6b83ac1e7931a295cbdaa6d3754e4ce5b6bb3386
GitHub Action <action@github.com>
2023-10-04 14:39:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/13x1/twpp/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/13x1/twpp/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.13x1.twpp.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/13x1/twpp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/13x1/twpp/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.13x1.twpp.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.13x1.twpp.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
